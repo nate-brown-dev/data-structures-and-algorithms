b1dc39f3a429ee9bf472f4a168b554170e9495a7
Nate Brown <nathan.d.brown@gmail.com>
2023-05-11 12:08:20 -0700

    bubble sort first pass

--- sorting/comparisons/sort.js
+++ sorting/comparisons/sort.js
@@ -1,6 +1,12 @@
 'use strict';
 
-const movies = require('./movies');
+const moviesObject = require('./movies');
+
+const moviesArray = moviesObject.Movies;
+
+console.log(moviesArray);
+
+// console.log(moviesArray);
 
 function sortYear(arr) {
   // sort code here
@@ -8,6 +14,25 @@ function sortYear(arr) {
 
 
 function sortTitle(arr) {
+  let n = arr.length;
+  let j = 0;
+  for ( let i = 0; i < n; i++ ) {
+    let left = arr[j]
+    let right = arr[j+1]
+    // console.log(left);
+    // console.log(right);
+    if ( right && left.title > right.title ) {
+      arr[j] = right;
+      arr[j+1] = left;
+      j++;
+    } else {
+    j++;
+    }
+  }
   // sort code here
+  // console.log(arr);
+  return arr;
 }
 
+// console.log(movies)
+console.log(sortTitle(moviesArray));
