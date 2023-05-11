b8f5a10c7402fe23f7c40b47008cb9a5dec203ef
Nate Brown <nathan.d.brown@gmail.com>
2023-05-11 12:10:37 -0700

    bubble sort first pass year

--- sorting/comparisons/sort.js
+++ sorting/comparisons/sort.js
@@ -6,12 +6,28 @@ const moviesArray = moviesObject.Movies;
 
 console.log(moviesArray);
 
-// console.log(moviesArray);
-
 function sortYear(arr) {
+  let n = arr.length;
+  let j = 0;
+  for ( let i = 0; i < n; i++ ) {
+    let left = arr[j]
+    let right = arr[j+1]
+    // console.log(left);
+    // console.log(right);
+    if ( right && left.year > right.year ) {
+      arr[j] = right;
+      arr[j+1] = left;
+      j++;
+    } else {
+    j++;
+    }
+  }
   // sort code here
+  // console.log(arr);
+  return arr;
 }
 
+console.log(sortYear(moviesArray));
 
 function sortTitle(arr) {
   let n = arr.length;
